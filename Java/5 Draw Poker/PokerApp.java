136e4f72d441f70eacc2e931fc49f4f7adfee062
Hunter Herbst <darkwaterkiller@gmail.com>
2020-08-12 23:37:29 -0400

    betting ui

--- Java/5 Draw Poker/PokerApp.java
+++ Java/5 Draw Poker/PokerApp.java
@@ -1,20 +1,29 @@
-import java.awt.event.*;
+import java.awt.ComponentOrientation;
+
 import javax.swing.*;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.EtchedBorder;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 
 public class PokerApp extends JFrame {
     
     private static final long serialVersionUID = 1L;
 
     private GroupLayout layout, bettingLayout;
+    private JButton incBetButton, decBetButton, betButton;
+    private JLabel pointsLabel, betLabel;
     private JPanel bettingPanel;
-    private JButton testButton;
-    private JToggleButton testToggle;
+    private JTextArea pointTextArea;
+    private JTextPane betTextPane;
 
     private Game g;
+    private int bet, points;
 
     public PokerApp() {
+        bet = 1;
+        points = 100;
+        
         initComponents();
         initListeners();
 
@@ -25,8 +34,17 @@ public class PokerApp extends JFrame {
     private void initComponents() {
         
         bettingPanel = new JPanel();
-        testButton = new JButton( "Print Toggle" );
-        testToggle = new JToggleButton( "Toggle Me" );
+        betLabel = new JLabel( "Bet Amount" );
+        decBetButton = new JButton( "-" );
+        incBetButton = new JButton( "+" );
+        betButton = new JButton( "Bet" );
+
+        betTextPane = new JTextPane();
+        SimpleAttributeSet betTextPaneAttributes = new SimpleAttributeSet();
+        StyleConstants.setAlignment( betTextPaneAttributes, StyleConstants.ALIGN_CENTER );
+        betTextPane.setParagraphAttributes( betTextPaneAttributes, true );
+        betTextPane.setText( String.valueOf( bet ) );
+        betTextPane.setEditable( false );
 
         this.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
         this.setTitle( "5 Draw Poker" );
@@ -53,18 +71,29 @@ public class PokerApp extends JFrame {
         bettingLayout.setHorizontalGroup(
             bettingLayout.createSequentialGroup()
             .addContainerGap()
-            .addComponent( testButton )
-            .addPreferredGap( ComponentPlacement.RELATED )
-            .addComponent( testToggle )
+            .addGroup(
+                bettingLayout.createParallelGroup( GroupLayout.Alignment.CENTER, true )
+                .addComponent( betLabel )
+                .addComponent( betTextPane )
+                .addGroup( GroupLayout.Alignment.CENTER, 
+                    bettingLayout.createSequentialGroup()
+                    .addComponent( decBetButton )
+                    .addComponent( betButton )
+                    .addComponent( incBetButton )
+                )
+            )
             .addContainerGap()
         );
         bettingLayout.setVerticalGroup(
             bettingLayout.createSequentialGroup()
             .addContainerGap()
+            .addComponent( betLabel )
+            .addComponent( betTextPane )
             .addGroup(
                 bettingLayout.createParallelGroup()
-                .addComponent( testButton )
-                .addComponent( testToggle )
+                .addComponent( decBetButton )
+                .addComponent( betButton )
+                .addComponent( incBetButton )
             )
             .addContainerGap()
         );
@@ -74,12 +103,28 @@ public class PokerApp extends JFrame {
     }
 
     private final void initListeners() {
-        testButton.addActionListener(
-            new ActionListener() {
-                @Override
-                public void actionPerformed( ActionEvent e ) {
-                    System.out.println(e);
-                }
+        // testButton.addActionListener(
+        //     new ActionListener() {
+        //         @Override
+        //         public void actionPerformed( ActionEvent e ) {
+                    
+        //         }
+        //     }
+        // );
+
+        decBetButton.addActionListener(
+            l -> {
+                if( bet > 1 )
+                    bet--;
+                betTextPane.setText( String.valueOf( bet ) );
+            }
+        );
+
+        incBetButton.addActionListener(
+            l -> {
+                if( bet < 5 )
+                    bet++;
+                betTextPane.setText( String.valueOf( bet ) );
             }
         );
     }
