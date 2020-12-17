08cd17f4447cb72ef150b209534e3b99a8b2de5a
Hunter Herbst <hunter.d.herbst2k@gmail.com>
2020-12-17 02:23:48 -0500

    made small ttt even smaller

--- C Programming/Small TicTacToe/smalltictactoe.c
+++ C Programming/Small TicTacToe/smalltictactoe.c
@@ -1,53 +1,51 @@
 #include <stdio.h>
 
-int b[] = {[0 ... 8]=32};
-int t = 1;
+int b[]={[0 ... 8]=32};
+int t=1;
 
 int getInp(){
     int inp;
-    printf("\n%c's move\nEnter a number 1 through 9 for where you want to make your move\n", t?88:79);
-    scanf("%d", &inp);
+    printf("\n%c's move\nEnter a number 1 through 9 for where you want to make your move\n",t?88:79);
+    scanf("%d",&inp);
     return inp;
 }
 
 int move(int m){
-    if( m < 1 || m > 9 || b[--m] != 32 ){
+    if(m<1||m>9||b[--m]!=32){
         puts("You cannot make that move!");
         return 1;
     }
-    b[m] = t?88:79;
-    t = !t;
+    b[m]=t?88:79;
+    t=!t;
     return 0;
 }
 
 int noFull(){
-    int i;
-    for( i = 0; i < 9; ++i )
-        if( b[i] == 32 )
+    for(int i=0;i<9;i++)
+        if(b[i]==32)
             return 1;
     return 0;
 }
 
 int noWin(){
-    int i;
-    if( ( ( b[4] == b[0] && b[4] == b[8] ) || ( b[4] == b[2] && b[4] == b[6] ) ) && b[4] != 32 )
+    if(((b[4]==b[0]&&b[4]==b[8])||(b[4]==b[2]&&b[4]==b[6]))&&b[4]!=32)
         return 0;
-    for( i = 0; i < 3; ++i )
-        if( ( b[i*3] == b[i*3+1] && b[i*3] == b[i*3+2] && b[i*3] != 32 ) || ( b[i] == b[i+3] && b[i] == b[i+6] && b[i] != 32 ) )
+    for(int i=0;i<3;i++)
+        if((b[i*3]==b[i*3+1]&&b[i*3]==b[i*3+2]&&b[i*3]!=32)||(b[i]==b[i+3]&&b[i]==b[i+6]&&b[i]!=32))
             return 0;
     return 1;
 }
 
 int main(void){
-    puts("   |   |\n-----------\n   |   |\n-----------\n   |   |");
-    while(noFull() && noWin()){
+    puts(" | |\n-----\n | |\n-----\n | |");
+    while(noFull()&&noWin()){
         while(move(getInp()));
-        printf("\n %c | %c | %c\n-----------\n %c | %c | %c\n-----------\n %c | %c | %c\n", b[6], b[7], b[8], b[3], b[4], b[5], b[0], b[1], b[2]);
+        printf("\n%c|%c|%c\n-----\n%c|%c|%c\n-----\n%c|%c|%c\n",b[6],b[7],b[8],b[3],b[4],b[5],b[0],b[1],b[2]);
     }
-    if(!noWin())
-        printf("%c wins!\n", t?79:88);
-    else
+    if(noWin())
         puts("Tie game!");
+    else
+        printf("%c wins!\n",t?79:88);
     puts("\nPress Enter to exit");
     while(getchar()!=10);
     getchar();
