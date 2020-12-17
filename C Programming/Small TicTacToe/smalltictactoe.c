c9a216551619c016f3a7ee4aeb86ff14c3e1a56b
Hunter Herbst <hunter.d.herbst2k@gmail.com>
2020-12-17 02:08:07 -0500

    Made small ttt smaller

--- C Programming/Small TicTacToe/smalltictactoe.c
+++ C Programming/Small TicTacToe/smalltictactoe.c
@@ -1,27 +1,7 @@
 #include <stdio.h>
 
-int getInp();
-int move(int);
-int noFull();
-int noWin();
 int b[] = {[0 ... 8]=32};
 int t = 1;
-
-int main(void){
-    puts("   |   |\n-----------\n   |   |\n-----------\n   |   |");
-    while(noFull() && noWin()){
-        while(move(getInp()));
-        printf("\n %c | %c | %c\n-----------\n %c | %c | %c\n-----------\n %c | %c | %c\n", b[6], b[7], b[8], b[3], b[4], b[5], b[0], b[1], b[2]);
-    }
-    if(!noWin())
-        printf("%c wins!\n", t?79:88);
-    else
-        puts("Tie game!");
-    puts("\nPress Enter to exit");
-    while(getchar()!=10);
-    getchar();
-    return 0;
-}
 
 int getInp(){
     int inp;
@@ -56,4 +36,20 @@ int noWin(){
         if( ( b[i*3] == b[i*3+1] && b[i*3] == b[i*3+2] && b[i*3] != 32 ) || ( b[i] == b[i+3] && b[i] == b[i+6] && b[i] != 32 ) )
             return 0;
     return 1;
+}
+
+int main(void){
+    puts("   |   |\n-----------\n   |   |\n-----------\n   |   |");
+    while(noFull() && noWin()){
+        while(move(getInp()));
+        printf("\n %c | %c | %c\n-----------\n %c | %c | %c\n-----------\n %c | %c | %c\n", b[6], b[7], b[8], b[3], b[4], b[5], b[0], b[1], b[2]);
+    }
+    if(!noWin())
+        printf("%c wins!\n", t?79:88);
+    else
+        puts("Tie game!");
+    puts("\nPress Enter to exit");
+    while(getchar()!=10);
+    getchar();
+    return 0;
 }
