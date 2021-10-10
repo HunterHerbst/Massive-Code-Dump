1165afe1f9b5277234854704077193b2bbc4e9c6
hunter_herbst <hunter.d.herbst2k@gmail.com>
2021-10-10 01:54:14 -0400

    more functions RVC

--- C/Random Value Counter/main.c
+++ C/Random Value Counter/main.c
@@ -3,17 +3,44 @@
 #include <time.h>
 
 #define MAX_NUM 100
-#define ITERATIONS 200
+#define ITERATIONS 2000
 
 int findMax(int* nums)
 {
-    //set max to the first number
+    //set max to the first number, get the size of the array
     int max = nums[0];
     //loop through the list of numbers
-    for(int i = 0; i < (sizeof(nums) / sizeof(nums[0])); i++)
+    for(int i = 0; i < MAX_NUM; i++)
         if(max < nums[i]) max = nums[i];
     //return the max number
     return max;
+}
+
+void printGraph(int* nums)
+{
+    printf("\t_");
+    for(int i = 0; i < findMax(nums); i++)
+        printf("_");
+    
+    for(int i = 0; i < MAX_NUM; i++)
+    {
+        if(nums[i] > 0)
+        {
+            printf("\n%-03d\t|", i);
+            for(int j = 0; j < nums[i]; j++)
+            {
+                printf("#");
+            }
+            printf("\t %d", nums[i]);
+        }
+    }
+}
+
+void printUnused(int* nums)
+{
+    printf("\nUnused numbers: ");
+    for(int i = 0; i < MAX_NUM; i++)
+        if(!nums[i]) printf("%d, ", i);
 }
 
 int main(char** argv, int argc)
@@ -28,9 +55,15 @@ int main(char** argv, int argc)
     for(int i = 0; i < ITERATIONS; i++) totalOcc[rand() % MAX_NUM]++;
 
     //print all values that occurred at least once
-    for(int i = 0; i < MAX_NUM; i++) 
-        if(totalOcc[i] > 0)
-            printf("%-03d:\t%-03d\n", i, totalOcc[i]);
+    // for(int i = 0; i < MAX_NUM; i++) 
+    //     if(totalOcc[i] > 0)
+    //         printf("%-03d:\t%-03d\n", i, totalOcc[i]);
+
+    //print the graph of all values
+    printGraph(totalOcc);
+
+    //print out the unused values
+    printUnused(totalOcc);
 
     //free memory and exit
     free(totalOcc);
